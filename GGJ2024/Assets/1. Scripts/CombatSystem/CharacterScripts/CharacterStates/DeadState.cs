de22979d28c033a70a87dde2f84316ffd6d19504
Danny-vD <dvduist@hotmail.nl>
2024-02-02 13:34:50 +0100

    Add a `DeadState` to the `Magus` prefab

---
+++ GGJ2024/Assets/1. Scripts/CombatSystem/CharacterScripts/CharacterStates/DeadState.cs
@@ -0,0 +1,28 @@
+﻿using CharacterScripts;
+using CombatSystem.Enums;
+
+namespace CombatSystem.CharacterScripts.CharacterStates
+{
+	public class DeadState : AbstractCharacterState
+	{
+		public override CharacterCombatStateType NextState => CharacterCombatStateType.Idle;
+		
+		private CharacterHealth characterHealth;
+
+		private void Awake()
+		{
+			characterHealth = GetComponent<CharacterHealth>();
+
+			characterHealth.OnResurrected += Exit;
+		}
+
+		private void OnDestroy()
+		{
+			characterHealth.OnResurrected -= Exit;
+		}
+
+		public override void Enter()
+		{
+		}
+	}
+}
