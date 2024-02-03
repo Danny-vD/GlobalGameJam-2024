2172ef66d16eb03b4db7a3a79f01c499c1c2bc4f
leopokoyo <leopokoyo@hotmail.com>
2024-02-03 13:55:49 +0100

    inventory stuff, addded (again)

--- GGJ2024/Assets/1. Scripts/Inventory/PlayerInventory.cs
+++ GGJ2024/Assets/1. Scripts/Inventory/PlayerInventory.cs
@@ -1,9 +1,45 @@
-﻿using VDFramework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ink.Runtime;
+using VDFramework;
 
 namespace Inventory
 {
-    public class Inventory : BetterMonoBehaviour
-    {
-        
-    }
+	public class PlayerInventory : BetterMonoBehaviour
+	{
+		private Dictionary<IItem, int> itemsList;
+
+		private void Awake()
+		{
+			itemsList = new Dictionary<IItem, int>();
+		}
+
+		public void AddItem(IItem newItem)
+		{
+			itemsList.TryAdd(newItem, 1);
+		}
+
+		public void RemoveItem(IItem toBeRemoved)
+		{
+			if (itemsList.ContainsKey(toBeRemoved))
+			{
+				itemsList.Remove(toBeRemoved);
+			}
+		}
+
+		public void PurgeItemList()
+		{
+			// Will crash?
+			foreach (KeyValuePair<IItem, int> keyValuePair in itemsList.Where(keyValuePair => keyValuePair.Value == 0))
+			{
+				itemsList.Remove(keyValuePair.Key);
+			}
+		}
+
+		public Dictionary<IItem, int> GetList()
+		{
+			return itemsList;
+		}
+	}
 }
