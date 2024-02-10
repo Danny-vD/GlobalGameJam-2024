a14f6a4c378baf913549bccabe63d737d533fa42
Danny-vD <dvduist@hotmail.nl>
2024-02-10 22:12:38 +0100

    Fix the mismapping of eventReferences to their enum on different Operating Systems (Assumingly caused by `EventManager.Events` returning the events in a different order)

--- GGJ2024/Assets/Editor/Utility/FMODUtilityPackage/EnumWriter/AudioEnumWriter.cs
+++ GGJ2024/Assets/Editor/Utility/FMODUtilityPackage/EnumWriter/AudioEnumWriter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using FMODUnity;
 using FMODUtilityPackage.Enums;
+using FMODUtilityPackage.Utility;
 using UnityEditor.Compilation;
 using UnityEngine;
 using Utility.EditorPackage;
@@ -24,20 +25,20 @@ namespace Utility.FMODUtilityPackage.EnumWriter
 		public static void WriteFmodEventsToEnum()
 		{
 			List<EditorEventRef> editorEventRefs = EventManager.Events;
-			string[] eventNames = editorEventRefs.Select(eventref => eventref.Path).ToArray();
+			string[] enumValues = editorEventRefs.Select(eventref => eventref.Path).ToArray();
 
-			string[] pathNames = new string[eventNames.Length];
-			Array.Copy(eventNames, pathNames, eventNames.Length);
+			string[] eventPaths = new string[enumValues.Length];
+			Array.Copy(enumValues, eventPaths, enumValues.Length);
 
-			WriteToResourcesUtil.WriteToResources(pathNames, "EventPaths.txt", "FMODUtilityPackage/");
+			WriteToResourcesUtil.WriteToResources(eventPaths, "EventPaths.txt", "FMODUtilityPackage/");
+			
+			enumValues = EventPathToEnumValueUtil.ConvertEventPathToEnumValuesString(enumValues);
 
-			WriteToFile(typePath, nameof(AudioEventType), eventNames, pathNames, '/'); // everything starts with 'event:/'
+			WriteToFile(typePath, nameof(AudioEventType), enumValues, eventPaths);
 		}
 
-		public static void WriteToFile(string path, string typeName, string[] values, string[] documentation, char startValueCharacter)
+		public static void WriteToFile(string path, string typeName, string[] values, string[] documentation)
 		{
-			values = ValidateArray(values, startValueCharacter);
-
 			WriteEnumValues(values, documentation, path, typeName);
 
 			CompilationPipeline.RequestScriptCompilation();
@@ -91,46 +92,6 @@ namespace Utility.FMODUtilityPackage.EnumWriter
 			builder.Append(afterEnum);
 
 			File.WriteAllText(fullPath, builder.ToString());
-		}
-
-		private static string[] ValidateArray(string[] array, char startCharacter)
-		{
-			List<string> list = array.Distinct().ToList();
-
-			for (int i = list.Count - 1; i >= 0; i--)
-			{
-				string value = list[i];
-
-				value = value.Substring(value.IndexOf(startCharacter) + 1);
-
-				if (value == string.Empty)
-				{
-					list.Remove(list[i]);
-					continue;
-				}
-
-				for (int j = value.Length - 1; j >= 0; j--)
-				{
-					char character = value[j];
-
-					// starting an enum value with a digit is not allowed, prefix it with 'E' if that is the case
-					if (j == 0 && char.IsDigit(value, 0))
-					{
-						value = "E" + value;
-						break;
-					}
-
-					// Replace any special characters with an underscore
-					if (!char.IsLetterOrDigit(character) && !character.Equals('_'))
-					{
-						value = value.Replace(character, '_');
-					}
-				}
-
-				list[i] = value;
-			}
-
-			return list.ToArray();
 		}
 	}
 }
