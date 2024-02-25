98a5ef1ad4a3c99f6a757d7f13c082d1d0924c0b
Danny-vD <dvduist@hotmail.nl>
2024-02-25 21:02:42 +0100

    Disable the move button if you do not have enough MP to cast that move

--- GGJ2024/Assets/1. Scripts/CombatSystem/UIScripts/CombatMoves/CombatMoveUIManager.cs
+++ GGJ2024/Assets/1. Scripts/CombatSystem/UIScripts/CombatMoves/CombatMoveUIManager.cs
@@ -8,40 +8,51 @@ using VDFramework;
 
 namespace CombatSystem.UIScripts.CombatMoves
 {
-    public class CombatMoveUIManager : BetterMonoBehaviour
-    {
-        [SerializeField] private Button selectMoveButton;
+	public class CombatMoveUIManager : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private Button selectMoveButton;
 
-        [SerializeField] private TMP_Text nameLabel;
+		[SerializeField]
+		private TMP_Text nameLabel;
 
-        [SerializeField] private TMP_Text descriptionLabel;
+		[SerializeField]
+		private TMP_Text descriptionLabel;
 
-        [SerializeField] private TMP_Text costLabel;
+		[SerializeField]
+		private TMP_Text costLabel;
 
-        private GameObject characterCaster;
+		private GameObject characterCaster;
 
-        private AbstractCombatMove combatMove;
+		private AbstractCombatMove combatMove;
 
-        public void Initialize(AbstractCombatMove move, GameObject character)
-        {
-            combatMove = move;
-            characterCaster = character;
+		public void Initialize(AbstractCombatMove move, GameObject character)
+		{
+			combatMove      = move;
+			characterCaster = character;
 
-            if (character.GetComponent<CharacterMP>().MP >= move.Cost) selectMoveButton.onClick.AddListener(SelectMove);
+			if (character.GetComponent<CharacterMP>().MP >= move.Cost)
+			{
+				selectMoveButton.onClick.AddListener(SelectMove);
+			}
+			else
+			{
+				selectMoveButton.interactable = false;
+			}
 
-            nameLabel.text = combatMove.AbilityName;
+			nameLabel.text = combatMove.AbilityName;
 
-            //descriptionLabel.text = combatMove.Description;
-            //costLabel.text        = combatMove.Cost.ToString();
-        }
+			//descriptionLabel.text = combatMove.Description;
+			//costLabel.text        = combatMove.Cost.ToString();
+		}
 
-        private void SelectMove()
-        {
-            // TODO make a targeting UI that hides the moves buttons, or change the way targetting works so another move can be selected without confirming
-            // at the moment clicking on another move after you selected one will both select that move and confirm the target at once, which causes the next confirm input to confim the move from the previous turn
-            CombatTargettingManager.Instance.ChooseMove(combatMove, characterCaster);
+		private void SelectMove()
+		{
+			// TODO make a targeting UI that hides the moves buttons, or change the way targetting works so another move can be selected without confirming
+			// at the moment clicking on another move after you selected one will both select that move and confirm the target at once, which causes the next confirm input to confim the move from the previous turn
+			CombatTargettingManager.Instance.ChooseMove(combatMove, characterCaster);
 
-            CombatMoveUISpawner.HideMoves();
-        }
-    }
+			CombatMoveUISpawner.HideMoves();
+		}
+	}
 }
