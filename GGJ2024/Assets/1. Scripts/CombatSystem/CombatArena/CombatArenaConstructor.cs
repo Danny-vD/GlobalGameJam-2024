adcf5886191f22801969622155d2ae43dd5a210a
Leopokoyo <leopokoyo@hotmail.com>
2024-03-02 07:12:40 +0100

    Xhangges to scenes and camera

---
+++ GGJ2024/Assets/1. Scripts/CombatSystem/CombatArena/CombatArenaConstructor.cs
@@ -0,0 +1,20 @@
+using System;
+using VDFramework;
+
+namespace CombatSystem.CombatArena
+{
+    public class CombatArenaConstructor : BetterMonoBehaviour
+    {
+        private void OnEnable()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDisable()
+        {
+            throw new NotImplementedException();
+        }
+        
+        
+    }
+}
