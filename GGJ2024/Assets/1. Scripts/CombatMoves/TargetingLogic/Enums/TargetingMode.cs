4da6a17dd440fca97c8799b1cda4506ccd981e4e
Danny-vD <dvduist@hotmail.nl>
2025-02-10 12:01:02 +0100

    Properly handle the TargettingMode of a move in the `CombatTargettingManager` and add clarification comments to the enum

--- GGJ2024/Assets/1. Scripts/CombatMoves/TargetingLogic/Enums/TargetingMode.cs
+++ GGJ2024/Assets/1. Scripts/CombatMoves/TargetingLogic/Enums/TargetingMode.cs
@@ -2,10 +2,10 @@
 {
     public enum TargetingMode
     {
-        TargetAll,
-        SingleTargetOnly,
-        MultipleTargets,
-        MultipleTargetsOnly,
-        None,
+        TargetAll, // Will auto-target all valid targets
+        SingleTargetOnly, // Can only target a single character
+        MultipleTargets, // Can target multiple, but does not *have* to
+        MultipleTargetsOnly, // Can only target multiple characters
+        None, // Cannot target anything (used for e.g. 'wait')
     }
 }
--- GGJ2024/Assets/1. Scripts/CombatSystem/Targeting/CombatTargettingManager.cs
+++ GGJ2024/Assets/1. Scripts/CombatSystem/Targeting/CombatTargettingManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using CombatMoves.ScriptableObjects.BaseClasses;
+using CombatMoves.TargetingLogic.Enums;
 using CombatSystem.CharacterScripts;
 using CombatSystem.Events.CharacterSelection;
 using CombatSystem.Managers;
@@ -23,11 +24,15 @@ namespace CombatSystem.Targeting
 		public event Action<GameObject> OnTargetAdded = delegate { };
 		public event Action<GameObject> OnTargetRemoved = delegate { };
 
+		public event Action OnTargetsValid = delegate { };
+		public event Action OnTargetsInvalid = delegate { };
+
 		[SerializeField] //TODO move to a seperate class
 		private GameObject currentSelectedCharacterIndicator;
 
 		public bool IsTargetingEnabled { get; private set; }
-		
+		public bool HasValidTargets { get; private set; }
+
 		private readonly List<GameObject> selectedTargets = new List<GameObject>();
 		private AbstractCombatMove currentMove;
 
@@ -37,7 +42,7 @@ namespace CombatSystem.Targeting
 		{
 			return CombatManager.GetAliveCombatParticipants().Where(participant => combatMove.IsValidTarget(participant, caster));
 		}
-		
+
 		protected override void Awake()
 		{
 			base.Awake();
@@ -51,17 +56,59 @@ namespace CombatSystem.Targeting
 
 			base.OnDestroy();
 		}
+		
+		private void EnableTargeting()
+		{
+			IsTargetingEnabled = true;
+			OnTargetingStarted.Invoke();
+			
+			EventManager.AddListener<CharacterClickedEvent>(OnCharacterClicked);
+		}
+
+		private void DisableTargeting(bool cancelledMove)
+		{
+			IsTargetingEnabled = false;
+			OnTargetingStopped.Invoke(cancelledMove);
+		}
 
 		public void EnableTargetingForMove(AbstractCombatMove combatMove, GameObject caster)
 		{
 			// BUG: left click confirms, but left click also selects a move | trying to select a move after you already selected a move simultaneously confirms the target and then selects the move (which causes you to select a move on someone who is not allowed yet to select a move)
 
+			if (IsTargetingEnabled)
+			{
+				DisableTargeting(true);
+			}
+			
 			selectedTargets.Clear();
+			SetHasValidTargetsFlag(false);
 
 			currentMove = combatMove;
+			
+			switch (currentMove.TargetingMode)
+			{
+				case TargetingMode.TargetAll:
+					AddTargets(GetAllValidTargets(combatMove, caster));
+					break;
+				case TargetingMode.SingleTargetOnly:
+				case TargetingMode.MultipleTargetsOnly:
+				case TargetingMode.MultipleTargets:
+					EnableTargeting();
+					break;
+				case TargetingMode.None:
+					CheckValidTargets();
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
 
-			IsTargetingEnabled = true;
-			OnTargetingStarted.Invoke();
+		private void AddTargets(IEnumerable<GameObject> targets)
+		{
+			foreach (GameObject target in targets)
+			{
+				AddTarget(target);
+			}
 		}
 
 		private void AddTarget(GameObject target)
@@ -81,9 +128,10 @@ namespace CombatSystem.Targeting
 			}
 
 			selectedTargets.Add(target);
-
-			OnTargetAdded.Invoke(target);
+			CheckValidTargets();
 			
+			OnTargetAdded.Invoke(target);
+
 			//TODO move to a seperate class
 			currentSelectedCharacterIndicator.transform.position = target.transform.position + Vector3.up * 1.5f;
 		}
@@ -93,6 +141,44 @@ namespace CombatSystem.Targeting
 			if (selectedTargets.Remove(target))
 			{
 				OnTargetRemoved.Invoke(target);
+			}
+		}
+
+		private bool CheckValidTargets()
+		{
+			switch (currentMove.TargetingMode)
+			{
+				case TargetingMode.TargetAll: // We auto-targetted all valid targets | Alternatively check selected targets against all valid targets
+					SetHasValidTargetsFlag(true);
+					break;
+				case TargetingMode.SingleTargetOnly:
+					SetHasValidTargetsFlag(selectedTargets.Count == 1);
+					break;
+				case TargetingMode.MultipleTargets:
+					SetHasValidTargetsFlag(selectedTargets.Count > 1);
+					break;
+				case TargetingMode.MultipleTargetsOnly:
+					SetHasValidTargetsFlag(selectedTargets.Count >= 1);
+					break;
+				case TargetingMode.None:
+					SetHasValidTargetsFlag(selectedTargets.Count == 0);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		private void SetHasValidTargetsFlag(bool hasValidTargets)
+		{
+			if (hasValidTargets && !HasValidTargets)
+			{
+				HasValidTargets = true;
+				OnTargetsValid.Invoke();
+			}
+			else if (!hasValidTargets && HasValidTargets)
+			{
+				HasValidTargets = false;
+				OnTargetsInvalid.Invoke();
 			}
 		}
 
@@ -120,8 +206,7 @@ namespace CombatSystem.Targeting
 
 			selectedTargets.Clear();
 
-			IsTargetingEnabled = false;
-			OnTargetingStopped.Invoke(false);
+			DisableTargeting(false);
 		}
 	}
 }
